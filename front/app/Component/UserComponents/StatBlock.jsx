570966e6c48c8fe2f395265b8d218d8fbfadb9d9
Sluch <ahmedking10710@gmail.com>
2025-09-12 04:28:57 +0300

    new commit edits in UI UX

--- front/app/Component/UserComponents/StatBlock.jsx
+++ front/app/Component/UserComponents/StatBlock.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const AnimatedCounter = ({ value = 0, duration = 0.8, className = '' }) => {
   const [display, setDisplay] = useState(0)
