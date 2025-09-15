add26173093e12d2135fdbd0cf8218d7f3b4f3b2
Sluch <ahmedking10710@gmail.com>
2025-09-15 14:40:14 +0300

    new edits v4 in UI UX

--- front/app/Component/MainApp.jsx
+++ front/app/Component/MainApp.jsx
@@ -25,7 +25,7 @@ const MainApp = () => {
       />
 
       {/* محتوى الصفحة */}
-      <div className="w-full px-3 pt-10 md:pt-6 pb-5">
+      <div className="w-full pt-10 md:pt-6 pb-5">
         {isLogin && <Stories />}
         <Sluchits />
       </div>
