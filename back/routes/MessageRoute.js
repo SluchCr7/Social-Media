d61292a9856ca3c6d72fcdeab203e43b9746f130
Sluch <ahmedking10710@gmail.com>
2025-09-21 04:23:50 +0300

    new edits v19

--- back/routes/MessageRoute.js
+++ back/routes/MessageRoute.js
@@ -14,19 +14,24 @@ const photoUpload = require('../Middelwares/uploadPhoto');
 route.route('/users')
     .get(verifyToken, getUsersInSideBar);
 
+// ✅ خلي unread قبل :id
+route.route('/messages/unread')
+    .get(verifyToken, getUnreadMessages);
+
 route.route('/messages/:id')
     .get(verifyToken, getMessages);
 
-route.route('/messages/unread') // ✅
-    .get(verifyToken, getUnreadMessages);
-
 route.route("/send/:id")
-    .post(verifyToken, photoUpload.fields([{ name: 'image', maxCount: 10 }]), sendMessage);
+    .post(
+      verifyToken,
+      photoUpload.fields([{ name: 'image', maxCount: 10 }]),
+      sendMessage
+    );
 
 route.route('/user/:id')
     .get(getMessagesByUser);
 
 route.route('/read/:id')
     .patch(verifyToken, makeAllMessagesIsReadBetweenUsers);
-
+    
 module.exports = route;
