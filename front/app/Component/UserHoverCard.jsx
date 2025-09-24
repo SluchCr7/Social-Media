d2cb33d74630ea580df3494a0532d45cc1a3c5c6
Sluch <ahmedking10710@gmail.com>
2025-09-24 09:43:57 +0300

    new edits v25

--- front/app/Component/UserHoverCard.jsx
+++ front/app/Component/UserHoverCard.jsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 import { useAuth } from '../Context/AuthContext'
 
 const UserHoverCard = ({ userSelected, children }) => {
-    if (!userSelected) return null;
     const {followUser , user} = useAuth()
+    if (!userSelected) return null;
   return (
     <HoverCard.Root>
       <HoverCard.Trigger asChild>
