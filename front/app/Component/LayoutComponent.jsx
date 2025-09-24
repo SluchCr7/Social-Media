d9eab952abd4d8b7a41367c9236c8479d038dd44
Sluch <ahmedking10710@gmail.com>
2025-09-24 21:29:07 +0300

    new edits v26

--- front/app/Component/LayoutComponent.jsx
+++ front/app/Component/LayoutComponent.jsx
@@ -84,7 +84,7 @@ const LayoutComponent = ({ children }) => {
             hideLayout
               ? `w-full `
               : isLogin
-              ? `w-full ${isCollapsed ? 'my-[85px]' : 'my-[260px]'}`
+              ? `w-full ${isCollapsed ? 'ml-[85px]' : 'ml-[260px]'}`
               : 'w-[90%] md:w-[80%] mx-auto'
           }`}
         >
