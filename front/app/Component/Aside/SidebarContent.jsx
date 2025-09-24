4d2f009c595f8a025a1e72c7f462f0641dab3e2d
Sluch <ahmedking10710@gmail.com>
2025-09-24 17:27:54 +0300

    new edits v26

--- front/app/Component/Aside/SidebarContent.jsx
+++ front/app/Component/Aside/SidebarContent.jsx
@@ -3,7 +3,7 @@ import { navSections } from '../../utils/Data'
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
-import { FiChevronLeft, FiX } from "react-icons/fi"
+import { memo } from 'react'
 
 const baseStyle = `relative flex items-center gap-3 px-3 py-2 rounded-lg text-sm font-medium transition-all duration-300 ease-in-out`
 const activeStyle = `bg-gradient-to-r from-indigo-500 to-purple-500 text-white shadow-md`
