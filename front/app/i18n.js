7b02183a0032a47d7562e0ff386f71c0a812a61d
Sluch <ahmedking10710@gmail.com>
2025-10-16 18:49:45 +0300

    Commit v37

--- front/app/i18n.js
+++ front/app/i18n.js
@@ -3,8 +3,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from '/locales/ar/translation.json';
-import ar from '/locales/ar/translation.json';
+import en from '@/public/locales/ar/translation.json';
+import ar from '@/public/locales/ar/translation.json';
 
 i18n
   .use(LanguageDetector)
