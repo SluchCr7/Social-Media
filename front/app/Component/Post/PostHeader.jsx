c055116b3d75f8ee11deccf19d4ea97f6ecb60d5
Sluch <ahmedking10710@gmail.com>
2026-02-02 00:14:02 +0200

    new responsive update

--- front/app/Component/Post/PostHeader.jsx
+++ front/app/Component/Post/PostHeader.jsx
@@ -26,7 +26,7 @@ const PostHeader = memo(({ post, user, isLogin, showMenu, setShowMenu, isCommuni
         {/* User Avatar - Professional Scale */}
         <UserHoverCard userSelected={owner}>
           <Link href={userProfileLink} className="relative w-12 h-12 rounded-2xl overflow-hidden border border-gray-100 dark:border-white/10 shrink-0 group">
-            <image
+            <Image
               width={500}
               height={500}
               src={owner?.profilePhoto?.url || '/default-profile.png'}
